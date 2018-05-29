5563852fd0065b38eda4b8c1c25fa1b23a2abafa
Modestas <ModestasK97@gmail.com>
2018-05-29 15:27:35 +0100

    mapping over favorite recipes and displaying them

--- src/components/FavoriteRecipeList.js
+++ src/components/FavoriteRecipeList.js
@@ -6,7 +6,16 @@ class FavoriteRecipeList extends Component {
   render() {
     <div>
       <h4>Favourite Recipes:</h4>
-
+      {
+        this.props.favoriteRecipes.map((recipe, i) => {
+          return (
+            <RecipeItem
+            key={i}
+            recipe={recipe}
+            />
+          )
+        })
+      }
     </div>
   }
 }
