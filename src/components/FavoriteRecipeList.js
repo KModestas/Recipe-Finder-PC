c54576b6319dd163a265041d4735173836aa83d9
Modestas <ModestasK97@gmail.com>
2018-05-29 15:20:19 +0100

    favorite recipe list component boilerplate

---
+++ src/components/FavoriteRecipeList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import RecipeItem from './RecipeItem';
+
+class FavoriteRecipeList extends Component {
+  render() {
+    <div></div>
+  }
+}
