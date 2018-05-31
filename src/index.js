ec5e20f1fa4693e37afb3023f1cd2190b1fce37b
Modestas <ModestasK97@gmail.com>
2018-05-31 16:48:02 +0100

    browser router routes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import rootReducer from './reducers';
+import FavoriteRecipeList from './components/FavoriteRecipeList';
 
 const store = createStore(rootReducer);
 store.subscribe(() => console.log('store', store.getState()));
 
+<BrowserRouter>
+  <Switch>
+    <Route exact path='/' component={App}>
+    </Route path='/favorites' component={FavoriteRecipeList} />
+  </Switch>
+</BrowserRouter>
 
 ReactDOM.render(
   <Provider store={store}>
